95064e2d190d59ab5d07d9c0f940c31af669e10f
MarcoszT <marcostitoq@gmail.com>
2025-10-19 21:36:41 -0500

    Initial Commit

---
+++ TaskHub/TaskHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskHubApp.swift
+//  TaskHub
+//
+//  Created by Marcos Tito on 19/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
