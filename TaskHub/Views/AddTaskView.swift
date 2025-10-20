1266dd162dcceeaec4b44753119bcf828113b8d5
MarcoszT <marcostitoq@gmail.com>
2025-10-19 22:05:41 -0500

    primer avance TuskHub app

---
+++ TaskHub/Views/AddTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTaskView.swift
+//  TaskHub
+//
+//  Created by Marcos Tito on 19/10/25.
+//
+
+import SwiftUI
+
+struct AddTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTaskView()
+}
