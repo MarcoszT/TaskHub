042c49c7813648c3d9f334e3c4b8070b24542af0
MarcoszT <marcostitoq@gmail.com>
2025-10-20 19:24:49 -0500

    Se agregan colores de app(beta), se crean componentes y navagacion de pantalla un poco mas clara. Se avanza un poco mas la pantalla principal (HomeView)

---
+++ TaskHub/ViewModels/DetailTask.swift
@@ -0,0 +1,18 @@
+//
+//  DetailTask.swift
+//  TaskHub
+//
+//  Created by Marcos Tito on 20/10/25.
+//
+
+import SwiftUI
+
+struct DetailTask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailTask()
+}
---
+++ TaskHub/ViewModels/TaskComponent.swift
@@ -0,0 +1,18 @@
+//
+//  TaskComponent.swift
+//  TaskHub
+//
+//  Created by Marcos Tito on 20/10/25.
+//
+
+import SwiftUI
+
+struct TaskComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskComponent()
+}
