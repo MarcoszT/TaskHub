c1dcc77da24fc09f04b52607e87380a919be867f
MarcoszT <marcostitoq@gmail.com>
2025-10-24 10:03:12 -0500

    creacion de bd usando coredata

---
+++ TaskHub/CoreData/TuskHubItem+CoreDataClass.swift
@@ -0,0 +1,17 @@
+//
+//  TuskHubItem+CoreDataClass.swift
+//  TaskHub
+//
+//  Created by Marcos Tito on 24/10/25.
+//
+//
+
+public import Foundation
+public import CoreData
+
+public typealias TuskHubItemCoreDataClassSet = NSSet
+
+@objc(TuskHubItem)
+public class TuskHubItem: NSManagedObject {
+
+}
