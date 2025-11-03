19b73d3fc3567694caf46426fbaed999d5e9577c
MarcoszT <marcostitoq@gmail.com>
2025-11-03 09:31:52 -0500

    creacion de mas detalles en vista y creacion de botones de acciones.

---
+++ TaskHub/Views/EditTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  EditTaskView.swift
+//  TaskHub
+//
+//  Created by Marcos Tito on 3/11/25.
+//
+
+import SwiftUI
+
+struct EditTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditTaskView()
+}
